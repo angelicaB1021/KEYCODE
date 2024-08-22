0b127e57d578ee0164afde99b4039531d30be14c
angelicaB1021 <rabeltran3@misena.edu.co>
2024-08-21 20:45:16 -0500

    select anidados

---
+++ Tareas/tarea_1.js
@@ -0,0 +1,10 @@
+// ========== TAREA ========== //
+const TablasDeMultiplicar = () => {
+for (let i = 1; inicio < i <= 10; i++ ){
+    console.log('Tabla de multiplicar ${i}:');
+for (let j = 1; j <= 10; j++) {
+console.log (`${i} x ${j} = ${ i*j}`);
+}
+console.log('tablasDeMultiplicar');
+}}
+
