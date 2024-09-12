ebe92302aef8a1996702fea7f046fb0a189e12fc
angelicaB1021 <rabeltran3@misena.edu.co>
2024-09-11 20:26:54 -0500

    .mns

--- select_anidados/src/components/SelectList.jsx
+++ select_anidados/src/components/SelectList.jsx
@@ -1,16 +1,16 @@
 export const SelectList = ({ title }) => {
-    const key = `select-${title}`;
-    const label = title.toUpperCase();
-    const options = ["Cund", "Anti", "Cald"];
-  
-    return (
-      <>
-      <div className="selectEstilos">
-      <label className="label-select" htmlFor={key}>{label}</label>
-      <select name={key} id={key}>
-          <option value="">Seleccione un {title}</option>
-        </select>
-      </div>   
-      </>
-    );
-  };
+  const key = `select-${title}`;
+  const label = title.toUpperCase();
+  const options = ["Cund", "Anti", "Cald"];
+
+  return (
+    <>
+    <div className="selectEstilos">
+    <label className="label-select" htmlFor={key}>{label}</label>
+    <select name={key} id={key}>
+        <option value="">Seleccione un {title}</option>
+      </select>
+    </div>   
+    </>
+  );
+};
